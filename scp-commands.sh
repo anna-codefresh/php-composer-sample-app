7d7c025899e19fdd9dc8195db8897ea1c54f5276
annabaker <anna.baker@codefresh.io>
2020-03-26 18:42:43 -0400

    Changes to scp

--- scp-commands.sh
+++ scp-commands.sh
@@ -7,4 +7,4 @@ PASSWORD=Loveme23
 echo | ssh-keygen -P '' -t rsa
 ssh-copy-id -i /root/.ssh/id_rsa.pub $USER@$HOST
                                                                                                   
-scp -r php-composer-sample-app $USER@$HOST:/home
+#scp -r php-composer-sample-app $USER@$HOST:/home
