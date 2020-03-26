54f54d2e046dfa83d52b957a2f01367581365989
annabaker <anna.baker@codefresh.io>
2020-03-26 19:08:58 -0400

    changes to scp

--- scp-commands.sh
+++ scp-commands.sh
@@ -5,6 +5,6 @@ HOST=192.81.218.202
 PASSWORD=Loveme23
 
 echo | ssh-keygen -P '' -t rsa
-ssh-copy-id -i /root/.ssh/id_rsa.pub -o StrictHostKeyChecking=no $USER@$HOST
+sshpass -p $PASSWORD ssh-copy-id -i /root/.ssh/id_rsa.pub -o StrictHostKeyChecking=no $USER@$HOST
 echo $PASSWORD                                                          
 scp -r php-composer-sample-app $USER@$HOST:/home
