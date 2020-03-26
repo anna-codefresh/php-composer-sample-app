0b51577749701d69fb625721e7ef600f2ca0d8ad
annabaker <anna.baker@codefresh.io>
2020-03-26 18:20:46 -0400

    Changes to scp commands

--- scp-commands.sh
+++ scp-commands.sh
@@ -5,5 +5,8 @@ HOST=192.81.218.202
 PASSWORD=Loveme23
 
 echo | ssh-keygen -P '' -t rsa
-yes yes | ssh-copy-id $USER@$HOST
+ssh-copy-id $USER@$HOST
+spawn step-commands.sh
+expect "Are you sure you want to continue connecting (yes/no)?"
+send "yes"                                                                                                    
 scp -i /root/.ssh/id_rsa -r php-composer-sample-app $USER@$HOST:/home
