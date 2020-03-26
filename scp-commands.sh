4ab824debf4c84d184ff4f634fde44b8402f4c4a
annabaker <anna.baker@codefresh.io>
2020-03-26 18:00:58 -0400

    fix to scp commands

--- scp-commands.sh
+++ scp-commands.sh
@@ -2,7 +2,9 @@
 
 USER=root
 HOST=192.81.218.202
+PASSWORD=Loveme23
 
 echo | ssh-keygen -P '' -t rsa
 ssh-copy-id $USER@$HOST
+yes | ./scp-commands.sh
 scp -i /root/.ssh/id_rsa -r php-composer-sample-app $USER@$HOST:/home
